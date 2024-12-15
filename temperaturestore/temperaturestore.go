e38e52543d5f05fc76dcd260f69ff40b14b09d34
Dmitriy <d>
2024-12-15 22:55:14 +0300

    add a body in the temperaturestore.go func

--- temperaturestore/temperaturestore.go
+++ temperaturestore/temperaturestore.go
@@ -1,6 +1,7 @@
 package temperaturestore
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -18,18 +19,96 @@ type TemperatureStore struct {
 	nextId       int
 }
 
-func New() *TemperatureStore
+func New() *TemperatureStore {
+	ts := &TemperatureStore{}
+	ts.temperatures = make(map[int]Temperature)
+	ts.nextId = 0
+	return ts
+}
 
-func (ts *TemperatureStore) CreateTemperature(value int, city string, datetime time.Time) int
+func (ts *TemperatureStore) CreateTemperature(value int, city string, datetime time.Time) int {
+	ts.Lock()
+	defer ts.Unlock()
 
-func (ts *TemperatureStore) GetTemperature(id int) (Temperature, error)
+	temperature := Temperature{
+		Id:       ts.nextId,
+		Value:    value,
+		City:     city,
+		Datetime: datetime,
+	}
+	ts.temperatures[ts.nextId] = temperature
+	ts.nextId++
+	return temperature.Id
+}
 
-func (ts *TemperatureStore) DeleteTemperature(id int) error
+func (ts *TemperatureStore) GetTemperature(id int) (Temperature, error) {
+	ts.Lock()
+	defer ts.Unlock()
 
-func (ts *TemperatureStore) DeleteAllTemperatures() error
+	temperature, ok := ts.temperatures[id]
+	if ok {
+		return temperature, nil
+	} else {
+		return Temperature{}, fmt.Errorf("temperature with id=%id not found", id)
+	}
+}
 
-func (ts *TemperatureStore) GetAllTemperatures() []Temperature
+func (ts *TemperatureStore) DeleteTemperature(id int) error {
+	ts.Lock()
+	defer ts.Unlock()
 
-func (ts *TemperatureStore) GetTemperaturesByCity(city string) []Temperature
+	if _, ok := ts.temperatures[id]; !ok {
+		return fmt.Errorf("temperature with id=%d not found")
+	}
 
-func (ts *TemperatureStore) GetTemperaturesByDatetime(year int, month time.Month, day int) []Temperature
+	delete(ts.temperatures, id)
+	return nil
+}
+
+func (ts *TemperatureStore) DeleteAllTemperatures() error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	ts.temperatures = make(map[int]Temperature)
+	return nil
+}
+
+func (ts *TemperatureStore) GetAllTemperatures() []Temperature {
+	ts.Lock()
+	defer ts.Unlock()
+
+	allTemperatures := make([]Temperature, 0, len(ts.temperatures))
+	for _, temperature := range ts.temperatures {
+		allTemperatures = append(allTemperatures, temperature)
+	}
+	return allTemperatures
+}
+
+func (ts *TemperatureStore) GetTemperaturesByCity(city string) []Temperature {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var temperatures []Temperature
+
+	for _, temperature := range ts.temperatures {
+		if temperature.City == city {
+			temperatures = append(temperatures, temperature)
+		}
+	}
+	return temperatures
+}
+
+func (ts *TemperatureStore) GetTemperaturesByDatetime(year int, month time.Month, day int) []Temperature {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var temperatures []Temperature
+
+	for _, temperature := range ts.temperatures {
+		y, m, d := temperature.Datetime.Date()
+		if y == year && m == month && d == day {
+			temperatures = append(temperatures, temperature)
+		}
+	}
+	return temperatures
+}
